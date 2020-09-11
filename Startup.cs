6f450a6c4d6a76edc0b3d58436b3568f70748c9b
Максим Васильчук <vasmax10@gmail.com>
2020-09-11 11:34:08 +0300

    work with azure sql added

--- Startup.cs
+++ Startup.cs
@@ -18,7 +18,7 @@ namespace web_lab1_fandom
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            string connection = Configuration.GetConnectionString("LocalConnection");
+            string connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<FandomContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
